d6ab4584a8824a4f8528e39162a529c023f59843
Vinayak Halvi <vinayakhalvi557@gmail.com>
2023-09-08 19:35:34 +0530

    Production commit

---
+++ _next/static/9SE3m-NdbCqhz_EcnInm1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/910-b79b5d8c24e05cc6.js","static/chunks/pages/index-197b05f1d7d62f17.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
